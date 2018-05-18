0142bb7df1fc0348e8680a2e16ceabf1b5f74174
Sean Hagan <sphagan1088@yahoo.com>
2018-05-17 17:32:35 -0700

    added more routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path"); 
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/contact.html"));
+    });
+
+    app.get("/portfolio", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+    });
+};
